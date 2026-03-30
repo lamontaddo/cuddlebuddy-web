b0ea5dd6475c11e7bd9112955f19cc664ce3bfff
lamont <lamontaddo@gmail.com>
2026-03-29 23:56:21 -0400

    fix: switch GitHub Pages build to custom domain base

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/cuddlebuddy-web">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
